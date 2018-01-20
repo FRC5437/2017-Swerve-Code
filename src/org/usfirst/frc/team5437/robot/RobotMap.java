602dac16f580d2c1b92e2b45b4d0f1feb9ff0998
Crockett Hopper <dachaeon@me.com>
2018-01-20 11:07:28 -0600

    replace magic numbers for encoder channels with named constants

--- src/org/usfirst/frc/team5437/robot/RobotMap.java
+++ src/org/usfirst/frc/team5437/robot/RobotMap.java
@@ -19,4 +19,12 @@ public class RobotMap {
 	final public static int REAR_LEFT_SWIVEL = 35;
 	final public static int REAR_RIGHT_SWIVEL = 45;
 	
+	final public static int FRONT_LEFT_ENCODER_CHANNEL_A = 0;
+	final public static int FRONT_LEFT_ENCODER_CHANNEL_B = 1;
+	final public static int FRONT_RIGHT_ENCODER_CHANNEL_A = 2;
+	final public static int FRONT_RIGHT_ENCODER_CHANNEL_B = 3;
+	final public static int REAR_LEFT_ENCODER_CHANNEL_A = 4;
+	final public static int REAR_LEFT_ENCODER_CHANNEL_B = 5;
+	final public static int REAR_RIGHT_ENCODER_CHANNEL_A = 6;
+	final public static int REAR_RIGHT_ENCODER_CHANNEL_B = 7;
 }
